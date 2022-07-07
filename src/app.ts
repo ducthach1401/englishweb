e23885bd4bec56378987a0dc11a4ea9a1eff34c6
ducthach.1401 <ducthach.1401@gmail.com>
2022-07-07 17:27:12 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,26 @@ import { CombineRoute } from "./modules/index";
 import config from "./config/config";
 import { routeView } from "./view/route/route.view";
 
-mongoose.connect(`${process.env.MONGO_URL}`, {
+mongoose
+  .connect(`${process.env.MONGO_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true, 
-    useFindAndModify: false
-}).then(async () =>{
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(async () => {
     const app: Application = express();
     const port = process.env.PORT || 5000;
-    
+
     config(app);
     const combineRoute = new CombineRoute();
     combineRoute.start(app);
     const viewRoute = new routeView();
     viewRoute.route(app);
     app.listen(port, () => {
-        console.log(`Server is running on ${port} at http://localhost:8080`);
+      console.log(`Server is running on ${port}`);
     });
-}).catch((err) => {console.log("Cannot connect to database. Error:", err);})
+  })
+  .catch((err) => {
+    console.log("Cannot connect to database. Error:", err);
+  });
